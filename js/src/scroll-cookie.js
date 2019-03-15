5120375202a0801cf44bf5c6ce65c7936fdafbac
minwenjun <minwenjun@didichuxing.com>
2019-03-15 23:24:39 +0800

    Site updated: 2019-03-15 23:24:38

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 15 2019 23:08:25 GMT+0800 (CST)
+// build time:Fri Mar 15 2019 23:24:26 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
