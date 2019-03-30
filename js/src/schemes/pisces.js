ebca54077441826beead730244d449effd95095c
aitozi <aitozi@aitozi.com>
2019-03-30 10:08:02 +0800

    Site updated: 2019-03-30 10:08:01

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 25 2019 23:34:37 GMT+0800 (CST)
+// build time:Sat Mar 30 2019 10:07:57 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
