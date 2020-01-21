edca8d8463a6d3f9981837c22f845866fa123eb4
愚鲤 <yuli.mwj@alibaba-inc.com>
2020-01-21 22:48:50 +0800

    Site updated: 2020-01-21 22:48:50

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Sep 15 2019 14:46:55 GMT+0800 (中国标准时间)
+// build time:Tue Jan 21 2020 22:48:43 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
