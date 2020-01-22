b8d6bfd11b2965e2b30a4e14739221284d523bf4
愚鲤 <yuli.mwj@alibaba-inc.com>
2020-01-22 17:49:34 +0800

    Site updated: 2020-01-22 17:49:33

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 21 2020 23:02:07 GMT+0800 (China Standard Time)
+// build time:Wed Jan 22 2020 17:49:28 GMT+0800 (China Standard Time)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
